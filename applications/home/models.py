3793f9888a89d4ee32b8c2c0d8af56f1eb38a656
JaimeHernandezE <jhearquitecto@gmail.com>
2023-06-27 22:34:33 -0400

    ADD: Contador de días en app home

--- applications/home/models.py
+++ applications/home/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from datetime import datetime
+
+
+class Countdown(models.Model):
+    end_date = models.DateField()
+
+    def get_days_left(self):
+        today = datetime.now().date()
+        days_left = (self.end_date - today).days
+        return days_left
