5835569500b78d6a490a60babd442f52d2d39acf
JaimeHernandezE <jhearquitecto@gmail.com>
2023-06-29 08:28:43 -0400

    ADD: Agregado login.html

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -6,5 +6,15 @@ app_name = 'users_app'
 
 urlpatterns = [
 
+    path(
+        'login/',
+        views.LoginUser.as_view(),
+        name = 'user-login',
+        ),
+    path(
+        'logout/',
+        views.LogoutView.as_view(),
+        name='user-logout',
+    ),
 
 ]
