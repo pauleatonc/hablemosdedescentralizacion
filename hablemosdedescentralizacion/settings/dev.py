ee37d227ebc123613e0709bbe9a1056eede9ac4b
Paul Eaton <paul.eaton@subdere.gov.cl>
2023-07-03 16:51:40 -0400

    WIP: Created caveunica app and integrated to the installed apps and settings prod, local, dev

---
+++ hablemosdedescentralizacion/settings/dev.py
@@ -0,0 +1,59 @@
+import environ
+from .base import *
+
+# we load the variables from the .env file to the environment
+env = environ.Env()
+environ.Env.read_env()
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-07n!hx-8et1p@wmnn0+w59x1g)qzxj^(m+)e&yrs#b6g7*m*-a'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    # Base de datos de aplicación
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("NAME"),
+        'USER': env("USER"),
+        'PASSWORD': env("PASSWORD"),
+        'HOST': env("HOST"),
+        'PORT': env("PORT"),
+        },
+    }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
+
+# EMAIL SETTINGS
+EMAIL_USE_TLS = True
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+
+# RECAPTCHA SETTINGS
+RECAPTCHA_PUBLIC_KEY = env("RECAPTCHA_PUBLIC_KEY")
+RECAPTCHA_PRIVATE_KEY = env("RECAPTCHA_PRIVATE_KEY")
+
+# SENDGRID SETTINGS
+SENDGRID_API_KEY = env("SENDGRID_API_KEY")
+ADMIN_EMAIL = env("ADMIN_EMAIL")
+NOREPLY_EMAIL = env("NOREPLY_EMAIL")
+
+#Clave Única
+CLAVE_UNICA_CLIENT_ID = env("CLAVE_UNICA_CLIENT_ID_DEV")
+CLAVE_UNICA_CLIENT_SECRET = env("CLAVE_UNICA_CLIENT_SECRET_DEV")
+CLAVE_UNICA_REDIRECT_URI = env("CLAVE_UNICA_REDIRECT_URI_DEV")
