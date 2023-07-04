a7034a2dbe2c8bbcc7e1ee64d7074b7e16eaea50
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-07-04 13:08:18 -0400

    crea vista preguntas-frecuentes

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -10,5 +10,17 @@ urlpatterns = [
         views.HomePageView.as_view(),
         name='index',
         ),
-    
+
+     path(
+        'preguntas-frecuentes',
+        views.PreguntasFrecuentesView.as_view(),
+        name='preguntas_frecuentes',
+        ),
+
+    path(
+        'documentos',
+        views.DocumentosView.as_view(),
+        name='documentos',
+        ),
+        
 ]
