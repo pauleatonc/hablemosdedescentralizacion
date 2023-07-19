7583f52e0c0e4d6910f57c46f252da31a2b047d0
vecarope <vecarope@mac-vcaro.local>
2023-07-18 21:42:51 -0400

    Add views news

--- applications/noticiasymedia/urls.py
+++ applications/noticiasymedia/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from . import views
+from .views import  NoticiaDetailView
 
 app_name = 'noticiasymedia_app'
 
 urlpatterns = [
     path('noticias/',views.NoticiasView.as_view(), name='noticias'), 
     path('multimedia/',views.MultimediaView.as_view(), name='multimedia'), 
-    path('noticias/<pk>/',views.NoticiasDetailView.as_view(), name='detalle noticia'), 
-    path('multimedia/<pk>/',views.MultimediaDetailView.as_view(), name='detalle multimedia'), 
+    path('noticias/<int:pk>/', NoticiaDetailView.as_view(), name='noticia_detail'),
+    path('multimedia/<pk>/',views.MultimediaDetailView.as_view(), name='detalle_multimedia'), 
 ]
