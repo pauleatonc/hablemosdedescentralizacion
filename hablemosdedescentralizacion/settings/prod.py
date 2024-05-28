20824da1fcb8b94bac9e52c736a94e17c9a10bb7
Rodrigo Zuñiga <rodrigo.zuniga@subdere.gov.cl>
2024-05-28 12:13:37 -0400

    chore: Agrega env variables a prod.py settings

--- hablemosdedescentralizacion/settings/prod.py
+++ hablemosdedescentralizacion/settings/prod.py
@@ -1,24 +1,18 @@
 import environ
+import os
 from .base import *
 
 # we load the variables from the .env file to the environment
 env = environ.Env()
-environ.Env.read_env()
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-07n!hx-8et1p@wmnn0+w59x1g)qzxj^(m+)e&yrs#b6g7*m*-a'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = [
-    'qahablemosdedescentralizacion.subdere.gob.cl',
-    'www.qahablemosdedescentralizacion.subdere.gob.cl',
-    'http://qahablemosdedescentralizacion.subdere.gob.cl',
-    'http://www.qahablemosdedescentralizacion.subdere.gob.cl',
-    '192.168.6.116',
-]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -31,21 +25,31 @@ DATABASES = {
         'PASSWORD': env("DB_PASSWORD"),
         'HOST': env("DB_HOST"),
         'PORT': env("DB_PORT"),
-        },
-    }
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.child('media')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILES_LOCATION = 'media'
+
+# Bucket Storage Configuration
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")  # e.g. us-west-2
+AWS_S3_FILE_OVERWRITE = env.bool('AWS_S3_FILE_OVERWRITE', default=False)
+AWS_DEFAULT_ACL = None
 
 # EMAIL SETTINGS
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
@@ -60,7 +64,15 @@ SENDGRID_API_KEY = env("SENDGRID_API_KEY")
 ADMIN_EMAIL = env("ADMIN_EMAIL")
 NOREPLY_EMAIL = env("NOREPLY_EMAIL")
 
-#Clave Única
-CLAVE_UNICA_CLIENT_ID = env("CLAVE_UNICA_CLIENT_ID_QA")
-CLAVE_UNICA_CLIENT_SECRET = env("CLAVE_UNICA_CLIENT_SECRET_QA")
-CLAVE_UNICA_REDIRECT_URI = env("CLAVE_UNICA_REDIRECT_URI_QA")
+# Clave Única
+CLAVE_UNICA_CLIENT_ID = env("CLAVE_UNICA_CLIENT_ID")
+CLAVE_UNICA_CLIENT_SECRET = env("CLAVE_UNICA_CLIENT_SECRET")
+CLAVE_UNICA_REDIRECT_URI = env("CLAVE_UNICA_REDIRECT_URI")
+
+# Trusted origins for the CSRF validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
+
+# If we are behind proxy with https we trust the header defined here.
+# https://docs.djangoproject.com/en/4.2/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = env.tuple('SECURE_PROXY_SSL_HEADER', default=None)
