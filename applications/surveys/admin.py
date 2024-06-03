6ea2ee1446aa3933f2d30d2d67e61a8759e84620
JaimeHernandezE <jhearquitecto@gmail.com>
2024-06-03 15:22:09 -0400

    FIX: Pregunta 5 actualizada

--- applications/surveys/admin.py
+++ applications/surveys/admin.py
@@ -1,24 +1,28 @@
 from django.contrib import admin
 
-from .models import PreguntaUno, PreguntaDos, PreguntaTres, PreguntaCuatro, PreguntaCinco, PreguntaSeis, PreguntaSiete, OpcionesPreguntaCinco
-
+from .models import PreguntaUno, PreguntaDos, PreguntaTres, PreguntaCuatro, PreguntaCinco, PreguntaSeis, PreguntaSiete, \
+    OpcionesPreguntaCinco
 
 
 @admin.register(PreguntaUno)
 class PreguntaUnoAdmin(admin.ModelAdmin):
     list_display = ['valor']
 
+
 @admin.register(PreguntaDos)
 class PreguntaDosAdmin(admin.ModelAdmin):
     list_display = ['propuesta_1', 'propuesta_2', 'propuesta_3', 'propuesta_4']
+
 
 @admin.register(PreguntaTres)
 class PreguntaTresAdmin(admin.ModelAdmin):
     list_display = ['iniciativa_1', 'iniciativa_2', 'iniciativa_3', 'iniciativa_4', 'iniciativa_5']
 
+
 @admin.register(PreguntaCuatro)
 class PreguntaCuatroAdmin(admin.ModelAdmin):
     list_display = ['tematica_1', 'tematica_2', 'tematica_3', 'tematica_4', 'tematica_5']
+
 
 @admin.register(PreguntaCinco)
 class PreguntaCincoAdmin(admin.ModelAdmin):
@@ -26,15 +30,19 @@ class PreguntaCincoAdmin(admin.ModelAdmin):
 
     def get_opciones(self, obj):
         return ", ".join([opcion.texto for opcion in obj.opciones.all()])
+
     get_opciones.short_description = 'Opciones Seleccionadas'
+
 
 @admin.register(PreguntaSeis)
 class PreguntaSeisAdmin(admin.ModelAdmin):
     list_display = ['valor']
 
+
 @admin.register(PreguntaSiete)
 class PreguntaCSieteAdmin(admin.ModelAdmin):
     list_display = ['texto_respuesta']
+
 
 @admin.register(OpcionesPreguntaCinco)
 class OpcionesPreguntaCincoAdmin(admin.ModelAdmin):
