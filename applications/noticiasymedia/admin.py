d8d9e2dddae84c256c4d687417cc88182c3140d9
JaimeHernandezE <jhearquitecto@gmail.com>
2024-07-01 08:36:57 -0400

    ADD: Foto y albumes

--- applications/noticiasymedia/admin.py
+++ applications/noticiasymedia/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models  import  Tag, Noticias, Multimedia
+from .models  import  Tag, Noticias, Multimedia,  PhotoAlbum, Photo
 
 # Register your models here.
 
@@ -9,3 +9,25 @@ admin.site.register(Tag)
 admin.site.register(Noticias)
 
 admin.site.register(Multimedia)
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    fields = ['foto', 'descripcion']
+    extra = 1  # Número de formas extras para cargar por defecto
+
+class PhotoAlbumAdmin(admin.ModelAdmin):
+    list_display = ['titulo_album', 'autor', 'region', 'date', 'public', 'photo_count']
+    list_filter = ['public', 'region', 'autor']
+    search_fields = ['titulo_album', 'descripcion_album']
+    inlines = [PhotoInline]
+
+    def photo_count(self, obj):
+        return obj.photo_set.count()
+    photo_count.short_description = 'Número de Fotos'
+
+    def save_model(self, request, obj, form, change):
+        if not obj.autor_id:  # Asigna el autor solo si es una nueva instancia
+            obj.autor = request.user
+        obj.save()
+
+admin.site.register(PhotoAlbum, PhotoAlbumAdmin)
--- applications/noticiasymedia/models.py
+++ applications/noticiasymedia/models.py
@@ -1,13 +1,16 @@
 # standard library
 from datetime import timedelta, datetime
+
+from django.core.exceptions import ValidationError
 #
 from django.db import models
 from django.template.defaultfilters import slugify
-# apps de terceros
-from model_utils.models import TimeStampedModel
-from imagekit.models import ImageSpecField
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+# apps de terceros
+from model_utils.models import TimeStampedModel
+
+from applications.regioncomuna.models import Region
 
 
 class Tag(models.Model):
@@ -77,3 +80,45 @@ class Multimedia(TimeStampedModel):
         self.slug = slugify(slug_unique)
         
         super(Multimedia, self).save(*args,**kwargs)
+
+
+class PhotoAlbum(TimeStampedModel):
+    titulo_album = models.CharField(max_length=200, verbose_name='Titulo Album (obligatorio)', null=True, blank=True)
+    descripcion_album = models.CharField(max_length=500, verbose_name='Descripcion Album (obligatorio)')
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, verbose_name='Region')
+    date = models.DateField(verbose_name='Fecha del Álbum')
+    foto_portada = ProcessedImageField(upload_to='album_photos', processors=[
+        ResizeToFill(1200, 630)], format='WEBP', options={'quality': 60}, null=True,
+        blank=False, verbose_name='Foto Portada (obligatorio)')
+    autor = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, verbose_name='Autor')
+    public = models.BooleanField(default=True, verbose_name='publico')
+
+    def __str__(self):
+        return self.titulo_album if self.titulo_album else "Álbum sin título"
+
+    class Meta:
+        ordering = ['region__region']
+        unique_together = ('region', 'date')
+
+
+class Photo(TimeStampedModel):
+    album = models.ForeignKey(
+        PhotoAlbum, on_delete=models.CASCADE, verbose_name='Album')
+    foto = ProcessedImageField(upload_to='album', processors=[
+        ResizeToFill(1200, 630)], format='WEBP', options={'quality': 60}, null=True,
+        blank=False, verbose_name='Foto (obligatorio)')
+    descripcion = models.CharField(
+        max_length=500, verbose_name='Descripcion Foto (obligatorio)')
+
+    def __str__(self):
+        return self.descripcion
+
+    def clean(self):
+        # Comprobar si el álbum ya tiene 12 fotos
+        if Photo.objects.filter(album=self.album).count() >= 12:
+            raise ValidationError(f"El álbum '{self.album.titulo_album}' no puede tener más de 12 fotos.")
+
+    def save(self, *args, **kwargs):
+        self.clean()  # Llama a clean para realizar la validación antes de guardar
+        super().save(*args, **kwargs)
