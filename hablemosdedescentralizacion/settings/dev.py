0774f0e695ac85c1c80ae524d2e0dfad854e9040
Paul Eaton <paul.eaton@subdere.gov.cl>
2024-07-04 11:42:58 -0400

    FIX: Static routes for QA deployment

--- hablemosdedescentralizacion/settings/dev.py
+++ hablemosdedescentralizacion/settings/dev.py
@@ -36,8 +36,8 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [BASE_DIR.child('static')]
+STATIC_ROOT = '/staticfiles/'
+STATICFILES_DIRS = [BASE_DIR.child('staticfiles')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
